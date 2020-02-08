8483d1ff75e87ea888d2a4601a527ecea3f0aacb
Nick Rimmer <xan@dipteam.com>
2020-02-08 16:58:48 +0200

    minor fix

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,14 +1,21 @@
 //import { updateExampleBgPreview } from './configuration.js';
 
+var askBeforeUnload = false;
+
 $(function(){
-    //updateExampleBgPreview();
+    window.onbeforeunload = function() {
+        if(askBeforeUnload) return "Changes you made may not be saved!";
+        return null;
+    }
+
+    $(".modal").parent("form").on("submit", modalSubmit);
 })
 
-function modalSubmit(form){
+function modalSubmit(e){
+    var form = e.target;
     var fn = $("[modal-save]", form).attr("modal-save");
     if(fn){
         var result = window[fn](form);
-        console.log(result);
         if(result !== false){
             $(".modal", form).modal('hide');
         }
