dee484410f0b347a75d9f8e1031c1ce691b8d434
Nick Rimmer <xan@dipteam.com>
2020-02-08 19:16:41 +0200

    js modules

--- assets/scripts/editor.js
+++ assets/scripts/editor.js
@@ -1,16 +1,27 @@
-var bgExamples = ["screen1.jpg", "screen2.jpg", "screen3.jpg", "screen4.jpg", "screen5.jpg", "screen6.jpg"];
-var bgExampleImage = bgExamples[0];
-var bgExampleWidth = 1280;
-var bgExampleHeight = 682;
+export let previewImages = ["screen1.jpg", "screen2.jpg", "screen3.jpg", "screen4.jpg", "screen5.jpg", "screen6.jpg"];
 
-$(function(){
-    updateBgExampleView();
+export let previewConfig = {
+    image: previewImages[0],
+    width: 1280,
+    height: 682
+};
+
+$(function () {
+    previewUpdateView();
 })
 
-function updateBgExampleView(){
+export function previewUpdate(image, width, height) {
+    previewConfig.image = image;
+    previewConfig.width= width;
+    previewConfig.height = height;
+
+    previewUpdateView();
+}
+
+function previewUpdateView(){
     $("#game-screen").css({
-        "background-image": "url(/assets/images/"+bgExampleImage+")",
-        "width": bgExampleWidth + "px",
-        "height": bgExampleHeight + "px"
+        "background-image": "url(/assets/images/" + previewConfig.image + ")",
+        "width": previewConfig.width + "px",
+        "height": previewConfig.height + "px"
     });
 }
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,5 +1,3 @@
-//import { updateExampleBgPreview } from './configuration.js';
-
 var askBeforeUnload = false;
 
 $(function(){
@@ -7,28 +5,4 @@ $(function(){
         if(askBeforeUnload) return "Changes you made may not be saved!";
         return null;
     }
-
-    $(".modal").parent("form").on("submit", modalSubmit);
-    $(".modal").on("show.bs.modal", modalShow);
 })
-
-function modalSubmit(e){
-    var form = e.target;
-    var fn = $("[modal-save]", form).attr("modal-save");
-    if(fn){
-        var result = window[fn](form);
-        if(result !== false){
-            $(".modal", form).modal('hide');
-        }
-    }else{
-        console.warn("Action wasn't specified");
-        console.warn(form);
-    }
-    return false;
-}
-
-function modalShow(e){
-    var form = e.target;
-    var fn = $("[modal-show]", form).attr("modal-show");
-    if(fn) window[fn](form);
-}
